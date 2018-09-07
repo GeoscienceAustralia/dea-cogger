1607ada0f710d2f353cee63f34aa6e10f9baca53
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-09-07 13:34:56 +1000

    COGWriter and JobControl classes implemented

---
+++ streamer/__init__.py
@@ -0,0 +1,1 @@
+from .streamer import JobControl, Streamer, COGWriter
--- streamer/streamer.py
+++ streamer/streamer.py
@@ -7,6 +7,14 @@ from os.path import join as pjoin, basename, dirname, exists
 import tempfile
 import subprocess
 from subprocess import check_call
+from netCDF4 import Dataset
+from datetime import datetime
+from pandas import to_datetime
+import gdal
+import xarray
+import yaml
+from yaml import CLoader as Loader, CDumper as Dumper
+import logging
 
 MAX_QUEUE_SIZE = 4
 WORKERS_POOL = 2
@@ -144,6 +152,201 @@ def upload_to_s3(item, src, dest, job_file):
     # Remove the file from the queue directory
     with tempfile.TemporaryDirectory() as tmpdir:
         run_command(['rm', src_name], tmpdir)
+
+
+class COGWriter(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _dataset_to_yaml(prefix, dataset, dest_dir):
+        y_fname = os.path.join(dest_dir, prefix + '.yaml')
+        dataset_object = dataset.decode('utf-8')
+        dataset = yaml.load(dataset_object, Loader=Loader)
+
+        # Update band urls
+        for key, value in dataset['image']['bands'].items():
+            value['layer'] = '1'
+            value['path'] = prefix + '_' + key + '.tif'
+
+        dataset['format'] = {'name': 'GeoTIFF'}
+        dataset['lineage'] = {'source_datasets': {}}
+        with open(y_fname, 'w') as fp:
+            yaml.dump(dataset, fp, default_flow_style=False, Dumper=Dumper)
+            logging.info("Writing dataset Yaml to %s", basename(y_fname))
+
+    @staticmethod
+    def datasets_to_yaml(file, dest_dir):
+        """ Refactored from Author Harshu Rampur's cog conversion scripts - Write the datasets to separate yaml files"""
+
+        file_names = JobControl.get_unstacked_names(file)
+        dataset_array = xarray.open_dataset(file)
+        for count in range(len(file_names)):
+            prefix, _, _ = file_names[count]
+            dataset_object = dataset_array.dataset.item(count)
+            COGWriter._dataset_to_yaml(prefix, dataset_object, dest_dir)
+
+    @staticmethod
+    def _dataset_to_cog(prefix, subdatasets, index, dest_dir):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            for dts in subdatasets[:-1]:
+                band_name = (dts[0].split(':'))[-1]
+                out_fname = prefix + '_' + band_name + '.tif'
+
+                env = ['GDAL_DISABLE_READDIR_ON_OPEN=YES',
+                       'CPL_VSIL_CURL_ALLOWED_EXTENSIONS=.tif']
+                subprocess.check_call(env, shell=True)
+
+                # copy to a tempfolder
+                temp_fname = pjoin(tmpdir, basename(out_fname))
+                to_cogtif = [
+                    'gdal_translate',
+                    '-of',
+                    'GTIFF',
+                    '-b',
+                    str(index),
+                    dts[0],
+                    temp_fname]
+                run_command(to_cogtif, tmpdir)
+
+                # Add Overviews
+                # gdaladdo - Builds or rebuilds overview images.
+                # 2, 4, 8,16,32 are levels which is a list of integral overview levels to build.
+                add_ovr = [
+                    'gdaladdo',
+                    '-r',
+                    'average',
+                    '--config',
+                    'GDAL_TIFF_OVR_BLOCKSIZE',
+                    '512',
+                    temp_fname,
+                    '2',
+                    '4',
+                    '8',
+                    '16',
+                    '32']
+                run_command(add_ovr, tmpdir)
+
+                # Convert to COG
+                cogtif = [
+                    'gdal_translate',
+                    '-co',
+                    'TILED=YES',
+                    '-co',
+                    'COPY_SRC_OVERVIEWS=YES',
+                    '-co',
+                    'COMPRESS=DEFLATE',
+                    '-co',
+                    'ZLEVEL=9',
+                    '--config',
+                    'GDAL_TIFF_OVR_BLOCKSIZE',
+                    '512',
+                    '-co',
+                    'BLOCKXSIZE=512',
+                    '-co',
+                    'BLOCKYSIZE=512',
+                    '-co',
+                    'PREDICTOR=2',
+                    '-co',
+                    'PROFILE=GeoTIFF',
+                    temp_fname,
+                    out_fname]
+                run_command(cogtif, dest_dir)
+
+    @staticmethod
+    def datasets_to_cog(file, dest_dir, message_queue=None):
+        """ Refactored from Author Harshu Rampur's cog conversion scripts - Write the datasets to separate cog files"""
+
+        file_names = JobControl.get_unstacked_names(file)
+        dataset = gdal.Open(file, gdal.GA_ReadOnly)
+        subdatasets = dataset.GetSubDatasets()
+        dataset_array = xarray.open_dataset(file)
+        for count in range(1, len(file_names) + 1):
+            prefix, _, _ = file_names[count - 1]
+            COGWriter._dataset_to_yaml(prefix, dataset_array.dataset.item(count - 1), dest_dir)
+            COGWriter._dataset_to_cog(prefix, subdatasets, count, dest_dir)
+            if message_queue:
+                message_queue.put(prefix, block=True)
+
+
+class JobControl(object):
+    def __init__(self):
+        pass
+
+    def aws_dir(self, file_name, aws_top_level: None):
+        """
+        This currently tested only for FC ls5, ls8
+        :param file_name: base NetCDF file name without directory structure,
+                          e.g. 'LS5_TM_FC_3577_-8_-22_1997_v20171127040509.nc'
+        :param aws_top_level: AWS specific top level part of the directory,
+                              e.g. /fractional_cover/fc/v2.2.0
+        :return: directory structure,
+                 e.g. ls5/x_-8/y_-22/1997 for the above LS5 file
+        """
+        pass
+
+    @staticmethod
+    def get_unstacked_names(netcdf_file, year=None, month=None):
+        """
+        warning: hard coded assumptions about FC file names here
+        :param netcdf_file: full pathname to a NetCDF file
+        :param year:
+        :param month:
+        :return:
+        """
+        dts = Dataset(netcdf_file)
+        file_id = os.path.splitext(basename(netcdf_file))[0]
+        prefix = "_".join(file_id.split('_')[0:-2])
+        stack_info = file_id.split('_')[-1]
+
+        dts_times = dts.variables['time']
+        names = []
+        for index, dt in enumerate(dts_times):
+            dt_ = datetime.fromtimestamp(dt)
+            time_stamp = to_datetime(dt_).strftime('%Y%m%d%H%M%S%f')
+            if year:
+                if month:
+                    if dt_.year == year and dt_.month == month:
+                        names.append(('{}_{}'.format(prefix, time_stamp), index, netcdf_file))
+                elif dt_.year == year:
+                    names.append(('{}_{}'.format(prefix, time_stamp), index, netcdf_file))
+            else:
+                names.append(('{}_{}'.format(prefix, time_stamp), index, netcdf_file))
+        return names
+
+    def get_dataset_names(self, src_dir, year=None, month=None):
+        """
+        warning: hard coded assumptions about FC file names here
+        :param src_dir:
+        :param year:
+        :param month:
+        :return:
+        """
+        names = []
+        for top, dirs, files in os.walk(src_dir):
+            for name in files:
+                name_ = os.path.splitext(name)
+                if name_[1] == '.nc':
+                    full_name = os.path.join(top, name)
+                    time_stamp = name_[0].split('_')[-2]
+                    # Is it a stacked NetCDF file
+                    if len(Dataset(full_name).variables['time']) > 1:
+                        if year:
+                            if int(time_stamp[0:4]) == year:
+                                names.extend(self.get_unstacked_names(full_name, year, month))
+                        else:
+                            names.extend(self.get_unstacked_names(full_name))
+                    else:
+                        if year:
+                            if int(time_stamp[0:4]) == year:
+                                if month:
+                                    if int(time_stamp[4:6]) == month:
+                                        names.append(("_".join(name_[0].split('_')[0:-1]), full_name))
+                                else:
+                                    names.append(("_".join(name_[0].split('_')[0:-1]), full_name))
+                        else:
+                            names.append(("_".join(name_[0].split('_')[0:-1]), full_name))
+        return names
 
 
 class Streamer(object):
