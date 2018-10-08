5670bc36905a339dc684656b86d3e1e3e183d361
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-10-08 17:40:10 +1100

    Separate processes for upload and cog runs

---
+++ streamer/upload_run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+module use /g/data/v10/public/modules/modulefiles
+module load dea
+
+python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py -p wofs_albers \
+                   -q /g/data/u46/users/aj9439/aws/queue \
+                   -j /g/data/u46/users/aj9439/aws/job \
+                   -f 0 14 \
+                   -y 2018 \
+                   -m 7 \
+                   --use_datacube \
+                   --datacube_env dea-prod \
+                   --reuse_full_list \
+                   --upload_only
