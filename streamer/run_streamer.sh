d33294e7b0a76b5d5a9090f6c117ee6cfc9c3062
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-10-10 15:53:40 +1100

    more cleanup

--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -2,4 +2,7 @@
 module use /g/data/v10/public/modules/modulefiles
 module load dea
 
+python streamer.py generate-work-list -p ls8_fc_albers -y 2018 -m 02 | \
+xargs python streamer.py convert_cog --num-procs 3 --output-dir $TMPDIR/test1 --product ls8_nbar_albers
+
 python streamer.py upload -p  wofs_albers
