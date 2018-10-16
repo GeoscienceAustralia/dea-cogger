8db77c68d6a821a58c78b2282103799e8db9ad47
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-10-16 13:35:22 +1100

    updated scripts

--- streamer/cog_run.sh
+++ streamer/cog_run.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
 module use /g/data/v10/public/modules/modulefiles
-module load dea
-export PYTHONUSERBASE=/g/data/u46/users/aj9439/local
+module load dea/20181015
 
+cat file_list | xargs python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py convert-cog \
+    --num-procs 4 --output-dir /g/data/u46/users/aj9439/aws/tmp --product ls8_fc_albers
 
-python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py -p wofs_albers \
-                   -q /g/data/u46/users/aj9439/aws/queue \
-                   -j /g/data/u46/users/aj9439/aws/job \
-                   -f 0 7 \
-                   -y 2018 \
-                   -m 7 \
-                   --use_datacube \
-                   --datacube_env dea-prod \
-                   --reuse_full_list \
-                   --cog_only
--- streamer/upload_run.sh
+++ streamer/upload_run.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
 module use /g/data/v10/public/modules/modulefiles
-module load dea
+module load dea/20181015
 
-python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py -p wofs_albers \
-                   -q /g/data/u46/users/aj9439/aws/queue \
-                   -j /g/data/u46/users/aj9439/aws/job \
-                   -f 0 7 \
-                   -y 2018 \
-                   -m 7 \
-                   --use_datacube \
-                   --datacube_env dea-prod \
-                   --reuse_full_list \
-                   --upload_only
+python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py upload \
+    --upload-destination s3://dea-public-data-dev/WOfS/filtered_summary/v2.1.0/combined
