7e18f8d7be4d46f0687aaffdfa5fb7a1f5c272dd
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-10-16 16:33:36 +1100

    updated scripts

--- streamer/upload_run.sh
+++ streamer/upload_run.sh
@@ -4,4 +4,5 @@ module use /g/data/v10/public/modules/modulefiles
 module load dea/20181015
 
 python /g/data/u46/users/aj9439/PycharmProjects/COG-Conversion/streamer/streamer.py upload \
+    --output-dir /g/data/u46/users/aj9439/aws/tmp \
     --upload-destination s3://dea-public-data-dev/WOfS/filtered_summary/v2.1.0/combined
