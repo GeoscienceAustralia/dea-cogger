a634c4cfc9816a00175f4c5f9e5e049c18ce7f34
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-12-17 13:10:35 +1100

    dataset prefix computed

--- work_list.py
+++ work_list.py
@@ -51,6 +51,7 @@ def get_indexed_files(product, year=None, month=None, from_date=None, datacube_e
     """
     Extract the file list corresponding to a product for the given year and month using datacube API.
     """
+    
     query = {'product': product}
     if from_date:
         query['time'] = Range(datetime(year=from_date.year, month=from_date.month, day=from_date.day),
@@ -61,30 +62,67 @@ def get_indexed_files(product, year=None, month=None, from_date=None, datacube_e
         query['time'] = Range(datetime(year=year, month=1, day=1), datetime(year=year + 1, month=1, day=1))
     dc = Datacube(app='streamer', env=datacube_env)
 
-    params = compile(CFG['products'][product]['prefix'])._named_fields
-    field_names = ['uri']
-    if 'x' in params or 'y' in params:
-        field_names.append('metadata_doc')
-    if 'year' in params or 'month' in params or 'day' in params:
-        field_names.append('time')
-    if 'lat' in params:
-        field_names.append('lat')
-    if 'lon' in params:
-        field_names.append('lon')
+    field_names = get_field_names(product)
     files = dc.index.datasets.search_returning(field_names=tuple(field_names), **query)
 
-    import ipdb; ipdb.set_trace()
     # Extract file name from search_result
     def filename_from_uri(uri):
         return uri.split('//')[1]
 
     for result in files:
+        prefix = compute_prefix_from_query_result(result, product)
+        yield filename_from_uri(result.uri)
 
-        # Get geo x and y values
-        metadata = result[2]
+
+def get_field_names(product):
+    """
+    Get field names for a datacube query for a given product
+    """
+    param_names = compile(CFG['products'][product]['prefix'])._named_fields
+    field_names = ['uri']
+    if 'x' in param_names or 'y' in param_names:
+        field_names.append('metadata_doc')
+    if 'year' in param_names or 'month' in param_names or 'day' in param_names:
+        field_names.append('time')
+    if 'lat' in param_names:
+        field_names.append('lat')
+    if 'lon' in param_names:
+        field_names.append('lon')
+    return field_names
+
+
+def compute_prefix_from_query_result(result, product):
+    """
+    Compute the AWS prefix corresponding to a dataset from datacube search result
+    """
+
+    param_names = compile(CFG['products'][product]['prefix'])._named_fields
+
+    params = {}
+
+    # Get geo x and y values
+    if hasattr(result, 'metadata_doc'):
+        metadata = result.metadata_doc
         geo_ref = metadata['grid_spatial']['projection']['geo_ref_points']['ll']
-        ref_x, ref_y = int(geo_ref['x']/100000), int(geo_ref['y']/100000)
-        yield filename_from_uri(result[0])
+        params['x'], params['y'] = int(geo_ref['x'] / 100000), int(geo_ref['y'] / 100000)
+
+    # Get lat and lon values
+    if hasattr(result, 'lat'):
+        params['lat'] = result.lat
+    if hasattr(result, 'lon'):
+        params['lon'] = result.lon
+
+    # Compute time values
+    if hasattr(result, 'time'):
+        mid_time = result.time.lower + (result.time.upper - result.time.lower) / 2
+        if 'year' in param_names:
+            params['year'] = mid_time.year
+        if 'month' in param_names:
+            params['month'] = mid_time.month
+        if 'day' in param_names:
+            params['day'] = mid_time.day
+
+    return CFG['products'][product]['prefix'].format(**params)
 
 
 if __name__ == '__main__':
