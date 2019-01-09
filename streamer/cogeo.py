05c5eac76666a6216ea0f1f29450f922be3631c3
Emma Ai <emma.ai@ga.gov.au>
2019-01-09 17:21:11 +1100

    fix nodata with int8

--- streamer/cogeo.py
+++ streamer/cogeo.py
@@ -1,11 +1,13 @@
 """rio_cogeo.cogeo: translate a file to a cloud optimized geotiff."""
 
+import os
 import sys
 
 import click
 
 import numpy
 
+import gdal
 import rasterio
 from rasterio.io import MemoryFile
 from rasterio.enums import Resampling
@@ -17,8 +19,6 @@ def cog_translate(
     dst_path,
     dst_kwargs,
     indexes=None,
-    nodata=None,
-    alpha=None,
     overview_level=5,
     overview_resampling=None,
     config=None,
@@ -37,10 +37,6 @@ def cog_translate(
         output dataset creation options.
     indexes : tuple, int, optional
         Raster band indexes to copy.
-    nodata, int, optional
-        nodata value for mask creation.
-    alpha, int, optional
-        alpha band index for mask creation.
     overview_level : int, optional (default: 6)
         COGEO overview (decimation) level
     config : dict
@@ -49,40 +45,41 @@ def cog_translate(
     """
     config = config or {}
 
+    nodata_mask = None
+    src = gdal.Open(src_path, gdal.GA_ReadOnly)
+    band = src.GetRasterBand(1)
+    nodata = band.GetNoDataValue() 
+    if band.DataType == gdal.GDT_Byte and  band.GetNoDataValue() < 0:
+            nodata_mask = 255
+    src = None
+
     with rasterio.Env(**config):
         with rasterio.open(src_path) as src:
 
             indexes = indexes if indexes else src.indexes
             meta = src.meta
             meta["count"] = len(indexes)
-            meta.pop("nodata", None)
             meta.pop("alpha", None)
 
             meta.update(**dst_kwargs)
             meta.pop("compress", None)
             meta.pop("photometric", None)
+            if nodata_mask is not None:
+                meta['nodata'] = nodata
+                meta['dtype'] = 'int16'
+            meta['stats'] = True
 
             with MemoryFile() as memfile:
                 with memfile.open(**meta) as mem:
                     wind = list(mem.block_windows(1))
                     for ij, w in wind:
                         matrix = src.read(window=w, indexes=indexes)
+                        if nodata_mask is not None:
+                            matrix = numpy.array(matrix, dtype='int16')
+                            matrix[matrix==nodata_mask] = nodata
+
                         mem.write(matrix, window=w)
-
-                        if nodata is not None:
-                            mask_value = (
-                                numpy.all(matrix != nodata, axis=0).astype(
-                                    numpy.uint8
-                                )
-                                * 255
-                            )
-                        elif alpha is not None:
-                            mask_value = src.read(alpha, window=w)
-                        else:
-                            mask_value = None
-                        if mask_value is not None:
-                            mem.write_mask(mask_value, window=w)
-
+                        
                     if overview_resampling is not None:
                         overviews = [2 ** j for j in range(1, overview_level + 1)]
 
