424121796de2af35421aacabd72656c62c7b6528
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-01-21 13:39:48 +1100

    Fix travis failures

--- check-code.sh
+++ check-code.sh
@@ -8,17 +8,17 @@ shopt -s globstar
 
 pylint -j 2 --reports no \
     COG-Conversion \
-    streamer/*.py
+    converter/*.py
 
 # E122: 'continuation line' has too many spurious errors.
 # E711: "is None" instead of "= None". Duplicates pylint check.
 # E701: "multiple statements on one line" is buggy as it doesn't understand py 3 types
 # E501: "line too long" duplicates pylint check
 pycodestyle --ignore=E122,E711,E701,E501 --max-line-length 120  \
-    streamer/*.py
+    converter/*.py
 
 # Finds shell scripts based on #!
-readarray -t SHELL_SCRIPTS < <(find streamer/*.sh -type f -exec file {} \; | grep "streamer" | cut -d: -f1)
+readarray -t SHELL_SCRIPTS < <(find converter/*.sh -type f -exec file {} \; | grep "converter" | cut -d: -f1)
 shellcheck -e SC1071,SC1090,SC1091 "${SHELL_SCRIPTS[@]}"
 
 # If yamllint is available, validate yaml documents
