f509d50d655a1b4a6357f40323fb908d6ed77171
Damien Ayers <damien@omad.net>
2020-03-27 17:21:53 +1100

    Fix pathnames in check-code.sh

--- check-code.sh
+++ check-code.sh
@@ -8,17 +8,17 @@ shopt -s globstar
 
 pylint -j 2 --reports no \
     COG-Conversion \
-    converter/*.py
+    dea_cogger/*.py
 
 # E122: 'continuation line' has too many spurious errors.
 # E711: "is None" instead of "= None". Duplicates pylint check.
 # E701: "multiple statements on one line" is buggy as it doesn't understand py 3 types
 # E501: "line too long" duplicates pylint check
 pycodestyle --ignore=E122,E711,E701,E501 --max-line-length 120  \
-    converter/*.py
+    dea_cogger/*.py
 
 # Finds shell scripts based on #!
-readarray -t SHELL_SCRIPTS < <(find converter/*.sh -type f -exec file {} \; | grep "dea_cogger" | cut -d: -f1)
+readarray -t SHELL_SCRIPTS < <(find dea_cogger/*.sh -type f -exec file {} \; | grep "dea_cogger" | cut -d: -f1)
 shellcheck -e SC1071,SC1090,SC1091 "${SHELL_SCRIPTS[@]}"
 
 # If yamllint is available, validate yaml documents
